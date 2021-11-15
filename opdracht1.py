d2e112d09062c1f4aab00c568d5640ea31851b2d
Matthijs4004 <matthijs4004@gmail.com>
2021-11-15 09:46:40 +0100

    opdracht 1

---
+++ opdracht1.py
@@ -0,0 +1,9 @@
+
+dieselprijs = float(input("Wat is de dieselprijs op dit moment in euro? "))
+# 1 liter = 12km
+# heen reis en terug reis 16 euro aan broodjes etc
+# 244km
+
+x = (244 / 12) * (dieselprijs) * 2 
+
+print(round(x, 2))
